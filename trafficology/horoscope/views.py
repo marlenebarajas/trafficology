aec3939c9ee3a938875f1c261b5c05bf12536561
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2024-01-15 16:41:05 -0800

    Update views.py

--- trafficology/horoscope/views.py
+++ trafficology/horoscope/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 
+from .models import Freeway
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the horoscope index.")
+    freeway_list = Freeway.objects.order_by("-id")
+
+    output = "\n".join([f.name for f in freeway_list])
+    return HttpResponse(output, content_type="text")
+
+# def freeways(request, id):
+#     response =
