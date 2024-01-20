d024c901473205fea07e91292d2722de7d1a954a
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2024-01-19 22:53:48 -0800

    Update urls.py

--- trafficology/trafficology/urls.py
+++ trafficology/trafficology/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("horoscope/", include("horoscope.urls")),
-    path("admin", admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
