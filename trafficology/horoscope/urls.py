30872a373ac200df6a1a9db09df9896f94cd7d0a
Marlene Barajas <48900002+marlenebarajas@users.noreply.github.com>
2024-01-19 23:04:09 -0800

    Add freeway list

--- trafficology/horoscope/urls.py
+++ trafficology/horoscope/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("daily", views.daily, name="daily"),
+    path("daily/", views.daily, name="daily"),
+    # path("freeways/", views.FreewayListView.as_view(), name="freeways"),
+    path("freeways/", views.freeways, name="freeways"),
 ]
 
 '''
--- trafficology/horoscope/views.py
+++ trafficology/horoscope/views.py
@@ -1,13 +1,11 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 from .models import Freeway
+from django.views import generic
 
 
 def index(request):
-    freeway_list = Freeway.objects.order_by("-id")
-
-    output = "\n".join([f.name for f in freeway_list])
-    return HttpResponse(output, content_type="text")
+    return render(request, "index.html")
 
 
 def daily(request):
@@ -16,5 +14,17 @@ def daily(request):
     output = "\n".join([f.name for f in freeway_list])
     return HttpResponse(output, content_type="text")
 
-# def freeways(request, id):
-#     response =
+
+def freeways(request):
+    freeway_names = Freeway.objects.all()
+
+    context = {
+        "freeway_names": freeway_names,
+    }
+    return render(request, "freeways/index.html", context=context)
+
+
+# class FreewayListView(generic.ListView):
+#     model = Freeway
+#     context_object_name = 'freeway_names'
+#     template_name = 'freeways/index.html'
